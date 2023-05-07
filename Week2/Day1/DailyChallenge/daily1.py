4b460fecfedb484a817aafcadc86dfded3a94645
ilyanosovsky <imnosovsky@gmail.com>
2023-05-07 14:49:37 +0300

    finished daily challenge

---
+++ Week2/Day1/DailyChallenge/daily1.py
@@ -0,0 +1,44 @@
+# Instructions
+# Using the input function, ask the user for a string. The string must be 10 characters long.
+# If it’s less than 10 characters, print a message which states “string not long enough”.
+# If it’s more than 10 characters, print a message which states “string too long”.
+
+# Then, print the first and last characters of the given text.
+
+# Using a for loop, construct the string character by character: Print the first character, then the second, then the third, until the full string is printed. For example:
+# The user enters "Hello World"
+# Then, you have to construct the string character by character
+# H
+# He
+# Hel
+# ... etc
+# Hello World
+
+
+text = input("Enter a string: ")
+if len(text) < 10:
+    print("String not long enough.")
+elif len(text) > 10:
+    print("String too long.")
+else:
+    for i in range(len(text)):
+        print(text[:i+1])
+
+
+# 4. Bonus: Swap some characters around then print the newly jumbled string (hint: look into the shuffle method). For example:
+
+# Hlrolelwod
+
+text = input("Enter a string: ")
+if len(text) < 10:
+    print("String not long enough.")
+elif len(text) > 10:
+    print("String too long.")
+else:
+    import random
+    text_list = list(text)
+    random.shuffle(text_list)
+    print("".join(text_list))
+    
+
+
