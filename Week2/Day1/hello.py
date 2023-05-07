f0c768f555e522450290ed622602db3e8846182f
ilyanosovsky <imnosovsky@gmail.com>
2023-05-07 14:08:37 +0300

    finished Exercise XP for day 1

--- Week2/Day1/hello.py
+++ Week2/Day1/hello.py
@@ -1,2 +1,22 @@
-msg = "hello world!"
-print(msg)
+# email = "jon@gmail.com"
+# position_of_at = email.find("@")
+# print(position_of_at)
+
+# user_name = input("What is your name? ")
+# print("Hello, " + user_name + "!")
+
+# In the python shell, Create a variable called my_age, use python to know how old you will be in 123879 years
+
+my_age = input("How old are you? ")
+print("In 123879 years, you will be " + str(int(my_age) + 123879) + " years old!")
+
+# ask a user for a number of miles, to convert kilometers and display it.
+
+miles = input("How many miles? ")
+print("That is " + str(float(miles) * 1.60934) + " kilometers!")
+
+# In the python shell, Create a variable called first_name and a variable called last_name, and then print your full name using those two variables
+
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+print("Your full name is " + first_name + " " + last_name + "!")
