9fd730920883012fc753da980797a45c9532c5bb
ilyanosovsky <imnosovsky@gmail.com>
2023-05-08 12:28:09 +0300

    finished classes exercises

--- Week2/Day2/class.py
+++ Week2/Day2/class.py
@@ -1,36 +1,51 @@
-colors = ["red", "blue",[1, 2, 3]]
-colors.append("green")   # Adds another element to the end of the list
-print(colors)
+# colors = ["red", "blue",[1, 2, 3]]
+# colors.append("green")   # Adds another element to the end of the list
+# print(colors)
 
 
-colors.insert(1, "yellow")  # Adds another element to the list at the specified index
-print(colors)
+# colors.insert(1, "yellow")  # Adds another element to the list at the specified index
+# print(colors)
 
-colors[3].insert(1, "orange") # Adds another element to the list at the specified index
-print(colors)
+# colors[3].insert(1, "orange") # Adds another element to the list at the specified index
+# print(colors)
 
-colors = ["red", "blue", "green"]
-answ = input("give position and color to insert")
-answ = answ.split(" ")
-colors.insert(int(answ[0]), answ[1])
-print(colors)
+# colors = ["red", "blue", "green"]
+# answ = input("give position and color to insert")
+# answ = answ.split(" ")
+# colors.insert(int(answ[0]), answ[1])
+# print(colors)
 
-colors = ["red", "blue", "green"]
-colors.pop()  # Removes the last element from the list
-print(colors)
+# colors = ["red", "blue", "green"]
+# colors.pop()  # Removes the last element from the list
+# print(colors)
 
-colors = ["red", "blue", "green"]
-colors.remove("blue")  # Removes the first element from the list that matches the given value
-print(colors)
+# colors = ["red", "blue", "green"]
+# colors.remove("blue")  # Removes the first element from the list that matches the given value
+# print(colors)
 
-colors = ["red", "blue", "green"]
-new_list = colors[0:2]  # Creates a new list from the given start and end index
-print(new_list)
+# colors = ["red", "blue", "green"]
+# new_list = colors[0:2]  # Creates a new list from the given start and end index
+# print(new_list)
 
-colors = ["red", "blue", "green"]
-new_list = colors[1:]  # Creates a new list from the given start index to the end of the list   
-print(new_list)
+# colors = ["red", "blue", "green"]
+# new_list = colors[1:]  # Creates a new list from the given start index to the end of the list   
+# print(new_list)
 
-colors = ["red", "blue", "green", "yellow", "orange", "purple"]
-new_list = colors[::2]  # Creates a new list from the given start and end index, skipping every second element
-print(new_list)
+# colors = ["red", "blue", "green", "yellow", "orange", "purple"]
+# new_list = colors[::2]  # Creates a new list from the given start and end index, skipping every second element
+# print(new_list)
+
+
+# list1 = [5, 10, 15, 20, 25, 50, 20]
+# list1[list1.index(20)] = 200
+# print(list1)
+
+# numbers = [100, 22, 31, 45, 500]
+# for i in numbers:
+#     if i % 2 == 0:
+#         print(f" {i} number is even")
+#     else :
+#         print(f" {i} number is odd")
+
+
+
