abdbf3a7df0f21011d9b64eecfa82675aa90805a
ilyanosovsky <imnosovsky@gmail.com>
2023-05-08 11:09:04 +0300

    doing exercises in the class

--- Week2/Day2/class.py
+++ Week2/Day2/class.py
@@ -14,3 +14,23 @@ answ = input("give position and color to insert")
 answ = answ.split(" ")
 colors.insert(int(answ[0]), answ[1])
 print(colors)
+
+colors = ["red", "blue", "green"]
+colors.pop()  # Removes the last element from the list
+print(colors)
+
+colors = ["red", "blue", "green"]
+colors.remove("blue")  # Removes the first element from the list that matches the given value
+print(colors)
+
+colors = ["red", "blue", "green"]
+new_list = colors[0:2]  # Creates a new list from the given start and end index
+print(new_list)
+
+colors = ["red", "blue", "green"]
+new_list = colors[1:]  # Creates a new list from the given start index to the end of the list   
+print(new_list)
+
+colors = ["red", "blue", "green", "yellow", "orange", "purple"]
+new_list = colors[::2]  # Creates a new list from the given start and end index, skipping every second element
+print(new_list)
