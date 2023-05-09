43fe3cbb2676b46cd48e4e79dfb89ec8b272eb1b
ilyanosovsky <imnosovsky@gmail.com>
2023-05-09 12:36:41 +0300

    finished class ex

--- Week2/Day3/class.py
+++ Week2/Day3/class.py
@@ -10,12 +10,27 @@
 # print(new_list)
 
 
-words = ['PYTHON', 'JAVA', 'Php', 'CoBoL', 'Cplus', 'C', 'JAVASCRIPT']
-new_list= []
-for item in words :
-    new_item = item.upper()
-    if new_item == 'PHP' :
-        continue   
-    new_list.append(new_item)
+# words = ['PYTHON', 'JAVA', 'Php', 'CoBoL', 'Cplus', 'C', 'JAVASCRIPT']
+# new_list= []
+# for item in words :
+#     new_item = item.upper()
+#     if new_item == 'PHP' :
+#         continue   
+#     new_list.append(new_item)
     
-print(new_list)
+# print(new_list)
+
+# dictionary contains list as value scores
+
+# games = {
+#     "score1" : 10,
+#     "score2" : 20,
+#     "score3" : 30,
+#     "score4" : 40
+# }
+
+# total = 0
+# for score in games.values() :
+#     total += score
+
+# print(total)
