1cc52765fe49ccb68b44e23484fe178964e0ad6c
ilyanosovsky <imnosovsky@gmail.com>
2023-05-10 21:42:42 +0300

    really finished XpGold

--- Week2/Day4/Exercises/XpGold.py
+++ Week2/Day4/Exercises/XpGold.py
@@ -38,5 +38,82 @@ else:
     print("You can't retire")
 
 
+# Exercise 2 : Sum
+# Instructions
+# Write a function that accepts one parameter (an int: X) and returns the value of X+XX+XXX+XXXX.
+# Example:
+# If X=3, the output when calling our function should be 3702 (3 + 33 + 333 + 3333)
+
+# Hint: treating our number as a int or a str at different points in our code may be helpful
+
+def sum_of_x(x):
+    sum = 0
+    for i in range(1, 5):
+        sum += int(str(x) * i)
+    return sum
+
+print(sum_of_x(3))
+
+
+# Exercise 3 : Double Dice
+# Instructions
+# Create a function that will simulate the rolling of a dice. Call it throw_dice. It should return an integer between 1 and 6.
+
+import random
+def throw_dice():
+    return random.randint(1, 6)
+
+print(throw_dice())
+
+# Create a function called throw_until_doubles.
+# It should keep throwing 2 dice (using your throw_dice function) until they both land on the same number, ie. until we reach doubles.
+# For example: (1, 2), (3, 1), (5,5) → then stop throwing, because doubles were reached.
+# This function should return the number of times it threw the dice in total. In the example above, it should return 3.
+
+def throw_until_doubles():
+    count = 0
+    while True:
+        count += 1
+        dice1 = throw_dice()
+        dice2 = throw_dice()
+        if dice1 == dice2:
+            return count
+        
+print(throw_until_doubles())
+
+# Create a main function.
+# It should throw doubles 100 times (ie. call your throw_until_doubles function 100 times), and store the results of those function calls (in other words, how many throws it took until doubles were thrown, each time) in a collection. 
+# (What kind of collection? Read below to understand what we will need the data for, and this should help you decide which data structure to use).
+
+# After the 100 doubles are thrown, print out a message telling the user how many throws it took in total to reach 100 doubles.
+# Also print out a message telling the user the average amount of throws it took to reach doubles. Round this off to 2 decimal places.
+# For example:
+
+# If the results of the throws were as follows (your code would do 100 doubles, not just 3):
+# (1, 2), (3, 1), (5, 5)
+# (3, 3)
+# (2, 4), (1, 2), (3, 4), (2, 2)
+
+# Then my output would show something like this:
+# Total throws: 8
+# Average throws to reach doubles: 2.67.
+
+
+
+
+def main():
+    count = 0
+    for i in range(100):
+        count += throw_until_doubles()
+    print(f"Total throws: {count}")
+    print(f"Average throws to reach doubles: {round(count / 100, 2)}")
+
+main()
+
+
+
+
+
+
 
 
