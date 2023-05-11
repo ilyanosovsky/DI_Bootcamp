b166670284fc40ddad1ab5e230e54477e6ae32e3
ilyanosovsky <imnosovsky@gmail.com>
2023-05-11 11:11:22 +0300

    did daily another way in class

--- Week2/Day5/class.py
+++ Week2/Day5/class.py
@@ -1,4 +1,29 @@
-ENGLISH = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0'
-MORSE = '·−  −···  −·−·  −··  ·  ··−·  −−·  ····  ··  ·−−−  −·−  ·−··  −−  −·  −−−  ·−−·  −−·−  ·−·  ···  −  ··−  ···−  ·−−  −··−  −·−−  −−··  ·−−−−  ··−−−  ···−−  ····−  ·····  −····  −−···  −−−··  −−−−·  −−−−−'
+secret = """7i3
+Tsi
+h%x
+i #
+sM 
+$a 
+#t%
+^r!"""
 
-dictionary = dict(zip(ENGLISH.split(), MORSE.split()))
+def create_matrix () :
+    list_one = secret.split("\n")
+    list_two = []
+    for num in range(3) :
+        list_two.append([char[num] for char in list_one])
+        # print(list_two)
+    return list_two
+# create_matrix()
+
+def create_sentence () :
+    lst = create_matrix()
+    sentence = ""
+    for column in lst :
+        for char in column :
+            if char.isalpha() :
+                sentence += char
+            else :
+                sentence += " "
+    print(sentence)
+create_sentence()
