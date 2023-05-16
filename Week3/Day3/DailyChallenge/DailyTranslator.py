397fc7e6c9cfcbf988bfb15c4c94e6d7ea618317
ilyanosovsky <imnosovsky@gmail.com>
2023-05-16 22:06:44 +0300

    finished  daily translate

--- Week3/Day3/DailyChallenge/DailyTranslator.py
+++ Week3/Day3/DailyChallenge/DailyTranslator.py
@@ -6,12 +6,21 @@
 # {"Bonjour": "Hello", "Au revoir": "Goodbye", "Bienvenue": "Welcome", "A bientôt": "See you soon"}
 # You have to recreate the result using a translator module.
 
-french_words= ["Bonjour", "Au revoir", "Bienvenue", "A bientôt"] 
+
 
 from translate import Translator
-translator= Translator(to_lang="English")
-for i in french_words:
-    translation = translator.translate(i)
-    print(translation)
+
+french_words = ["Bonjour", "Au revoir", "Bienvenue", "A bientôt"]
+translator = Translator(to_lang="en", from_lang="fr")
+
+
+def translate(word):
+    return translator.translate(word)
+
+
+result = {k: translate(k) for k in french_words}
+print(result)
+
+
 
     
