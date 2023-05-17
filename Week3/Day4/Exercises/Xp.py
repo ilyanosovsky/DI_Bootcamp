8884eef630b5d6f8201ddac40b2809d2729dbe7e
ilyanosovsky <imnosovsky@gmail.com>
2023-05-17 15:25:01 +0300

    working with XP

--- Week3/Day4/Exercises/Xp.py
+++ Week3/Day4/Exercises/Xp.py
@@ -42,3 +42,37 @@ def main():
     print(get_random_sentence(length))
 
 main()
+
+# Exercise 2: Working With JSON
+# Instructions
+
+
+import json
+sampleJson = """{ 
+   "company":{ 
+      "employee":{ 
+         "name":"emma",
+         "payable":{ 
+            "salary":7000,
+            "bonus":800
+         }
+      }
+   }
+}"""
+
+
+# Access the nested “salary” key from the JSON-string above.
+# Add a key called “birth_date” to the JSON-string at the same level as the “name” key.
+# Save the dictionary as JSON to a file.
+
+data = json.loads(sampleJson)
+print(data["company"]["employee"]["payable"]["salary"])
+
+data["company"]["employee"]["birth_date"] = "05/01/1994"
+with open("data.json", "w") as f:
+    json.dump(data, f, indent=4)
+
+    
+
+
+
