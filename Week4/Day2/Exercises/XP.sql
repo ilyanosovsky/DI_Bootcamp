2d00fbe46bc817290eabc901f2a8b7e9f0e03081
ilyanosovsky <imnosovsky@gmail.com>
2023-05-22 21:55:50 +0300

    finished XP

--- Week4/Day2/Exercises/XP.sql
+++ Week4/Day2/Exercises/XP.sql
@@ -45,25 +45,75 @@
 
 
 
-
 -- Write a query to get the address, and the phone number of all customers living in the Texas district, these details can be found in the “address” table.
+-- SELECT address, phone FROM address WHERE district = 'Texas' 
+
+
 
 -- Write a query to retrieve all movie details where the movie id is either 15 or 150.
+-- SELECT * FROM film WHERE film_id = 15 or film_id = 150
+
+
 
 -- Write a query which should check if your favorite movie exists in the database. Have your query get the film ID, title, description, length and the rental rate, these details can be found in the “film” table.
+-- SELECT film_id, title, description, length, rental_rate 
+-- FROM film 
+-- WHERE title = 'African Egg'
+
+
+
 
 -- No luck finding your movie? Maybe you made a mistake spelling the name. Write a query to get the film ID, title, description, length and the rental rate of all the movies starting with the two first letters of your favorite movie.
+-- SELECT film_id, title, description, length, rental_rate 
+-- FROM film 
+-- WHERE title ILIKE 'Fo%'
+
+
 
 -- Write a query which will find the 10 cheapest movies.
+-- SELECT title, replacement_cost
+-- FROM film 
+-- ORDER BY replacement_cost DESC LIMIT 10
+
 
 -- Not satisfied with the results. Write a query which will find the next 10 cheapest movies.
 -- Bonus: Try to not use LIMIT.
+-- SELECT title, replacement_cost
+-- FROM film 
+-- ORDER BY replacement_cost DESC LIMIT 10
+-- OFFSET 10
 
--- Write a query which will join the data in the customer table and the payment table. You want to get the first name and last name from the curstomer table, as well as the amount and the date of every payment made by a customer, ordered by their id (from 1 to…).
+
+-- Write a query which will join the data in the customer table and the payment table. 
+-- You want to get the first name and last name from the curstomer table, as well as the amount and the date of every payment made 
+-- by a customer, ordered by their id (from 1 to…).
+-- SELECT first_name, last_name, amount, payment_date  FROM customer
+-- INNER JOIN payment ON customer.customer_id = payment.customer_id ORDER BY customer.customer_id
+
+
+
+
 
 -- You need to check your inventory. Write a query to get all the movies which are not in inventory.
+-- SELECT * FROM film LEFT JOIN inventory ON inventory.film_id = film.film_id WHERE inventory.film_id IS NULL
+
+
 
 -- Write a query to find which city is in which country.
+-- SELECT city, country FROM city LEFT JOIN country ON city.country_id = country.country_id
 
--- Bonus You want to be able to see how your sellers have been doing? Write a query to get the customer’s id, names (first and last), the amount and the date of payment ordered by the id of the staff member who sold them the dvd.
+
+
+
+-- Bonus You want to be able to see how your sellers have been doing? 
+-- Write a query to get the customer’s id, names (first and last), the amount and the date of payment
+-- ordered by the id of the staff member who sold them the dvd.
+
+
+-- SELECT staff.staff_id, customer.first_name, customer.last_name, amount, payment_date 
+-- FROM staff 
+-- LEFT JOIN payment ON staff.staff_id = payment.staff_id 
+-- LEFT JOIN customer on payment.customer_id = customer.customer_id 
+-- ORDER BY staff_id
+
 
