b0de23d0a7e2e25bd52ceda14f9357e70a807580
ilyanosovsky <imnosovsky@gmail.com>
2023-05-24 16:14:25 +0300

    finished XP and fixed bugs

--- Week4/Day4/Exercises/menu_editor.py
+++ Week4/Day4/Exercises/menu_editor.py
@@ -35,58 +35,63 @@ def manage_connection(query):
 # Call the appropriate function that matches the user’s input.
 
 from menu_item import MenuItem
+from menu_manager import MenuManager
 
 
 def show_user_menu():
-    print("Welcome to the Menu Manager!")
-    print("What would you like to do?")
+    print("=====================================")
+    print("----Welcome to the Menu Manager!-----")
+    print("-------------------------------------")
+    print("-- What would you like to do? --")
     print("View an Item (V)")
     print("Add an Item (A)")
     print("Delete an Item (D)")
     print("Update an Item (U)")
     print("Show the Menu (S)")
     print("Exit (E)")
+    print("=====================================")
     user_input = input("Enter your choice: ").upper()
     return user_input
 
 def call_the_func(self):
     if self == "V":
         item_name = input("Enter item name: ")
-        item_price = input("Enter item price: ")
-        menu_item = MenuItem(item_name, item_price)
+        # item_price = input("Enter item price: ")
+        menu_item = MenuManager(item_name, None)
         result = menu_item.get_by_name()
         if result == None:
-            print("There is no such item in the menu.")
+            print("----- There is no such item in the menu.")
         else:
-            print(f"{result[0][1]} is {result[0][2]}$")
+            print(f"----- {result[0][1]} is {result[0][2]}$")
     elif self == "A":
         item_name = input("Enter item name: ")
         item_price = input("Enter item price: ")
         menu_item = MenuItem(item_name, item_price)
         menu_item.add_item()
-        print(f"{item_name} was added to the menu.")
+        print(f"----- {item_name} was added to the menu.")
     elif self == "D":
         item_name = input("Enter item name: ")
         item_price = input("Enter item price: ")
         menu_item = MenuItem(item_name, item_price)
         menu_item.delete_item()
-        print(f"{item_name} was deleted from the menu.")
+        print(f"----- {item_name} was deleted from the menu.")
     elif self == "U":
         item_name = input("Enter item name: ")
         item_price = input("Enter item price: ")
         menu_item = MenuItem(item_name, item_price)
         menu_item.update_item()
-        print(f"{item_name} was updated.")
+        print(f"----- {item_name} was updated.")
     elif self == "S":
-        menu_item = MenuItem(None, None)
+        menu_item = MenuManager(None, None)
         result = menu_item.all_items()
+        print("------MENU------")
         for item in result:
             print(f"{item[1]} is {item[2]}$")
     elif self == "E":
-        print("Goodbye!")
+        print("----- Goodbye!")
         exit()
     else:
-        print("Invalid input. Please try again.")
+        print("----- Invalid input. Please try again.")
         show_user_menu()
 
 while True:
