b3d0b289102d6a8210cc0b48a6c7c87401fc2b58
ilyanosovsky <imnosovsky@gmail.com>
2023-05-28 22:39:01 +0300

    finished almost DC

---
+++ Week5/Day1/DailyChallenge/dc_project_top/app/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+
+people_list = [
+  {
+    'id': 1,
+    'name': 'Bob Smith',
+    'age': 35,
+    'country': 'USA'
+  },
+  {
+    'id': 2,
+    'name': 'Martha Smith',
+    'age': 60,
+    'country': 'USA'
+  },
+  {
+    'id': 3,
+    'name': 'Fabio Alberto',
+    'age': 18,
+    'country': 'Italy'
+  },
+  {
+    'id': 4,
+    'name': 'Dietrich Stein',
+    'age': 85,
+    'country': 'Germany'
+  }
+]
+
+
+def people(request):
+    sorted_people = sorted(people_list, key=lambda x: x['age'])
+    results = {}
+    for index, value in enumerate(sorted_people):
+        results[index] = value
+    context = {}
+    context['results'] = results
+    return render(request, 'people.html', context)
+
+def person(request, id):
+    for i in people_list:
+        if i['id'] == int(id):
+            context = i
+    return render(request, 'person.html', context)
