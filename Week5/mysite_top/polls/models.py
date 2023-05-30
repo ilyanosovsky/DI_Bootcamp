d19dc1092d6d8475abeacb589810c225e8cee0e6
ilyanosovsky <imnosovsky@gmail.com>
2023-05-30 10:40:53 +0300

    work in class

--- Week5/mysite_top/polls/models.py
+++ Week5/mysite_top/polls/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
-    author = models.CharField(max_length=20)
+    author = models.ForeignKey(Person, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    category = models.ManyToManyField('Category')
 
 class Category(models.Model):
     name = models.CharField(max_length=20)
+
+
 
