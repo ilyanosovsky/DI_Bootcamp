144ee3e878cfd0af5d0e659ee684468cf1f9962a
ilyanosovsky <imnosovsky@gmail.com>
2023-06-01 14:16:13 +0300

    finished DC from day2

--- Week5/Day2/DailyChallenge/dc_project_top/app_numbers/models.py
+++ Week5/Day2/DailyChallenge/dc_project_top/app_numbers/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
-
 
 class Person(models.Model):
     name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50, unique=True)
-    phone_number = PhoneNumberField()
-    address = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+
 
--- Week5/Day2/DailyChallenge/dc_project_top/app_numbers/views.py
+++ Week5/Day2/DailyChallenge/dc_project_top/app_numbers/views.py
@@ -1,37 +1,20 @@
 from django.shortcuts import render
 from .models import Person
 
-def phonenumber(request, phone_number):
-    person_all = Person.objects.all()
-    if str(phone_number) in [str(p.phone_number) for p in person_all]:
-        for p in person_all:
-            if str(p.phone_number) == str(phone_number):
-                context = {
-                    'found': True,
-                    'name': p.name,
-                    'email': p.email,
-                    'address': p.address,
-                    'phone_number': p.phone_number
-                    }
-    else:
-        context = {}
-    
-    return render(request, 'phonenumber.html', context)
-
-
-def name(request, name):
-    person_all = Person.objects.all()
-    if name in [str(p.name) for p in person_all]:
-        for p in person_all:
-            if p.name == name:
-                context = {
-                    'found': True,
-                    'name': p.name,
-                    'email': p.email,
-                    'address': p.address,
-                    'phone_number': p.phone_number
-                    }
-    else:
-        context = {}
-    
+def by_name(request, name_search: str):
+    person = None
+    try:
+        person = Person.objects.get(name=name_search)
+    except Person.DoesNotExist:
+        pass
+    context = {'person': person}
     return render(request, 'name.html', context)
+
+def by_number(request, phonenumber: str):
+    person = None
+    try:
+        person = Person.objects.get(phone_number=phonenumber)
+    except Person.DoesNotExist:
+        pass
+    context = {'person': person}
+    return render(request, 'phonenumber.html', context)
