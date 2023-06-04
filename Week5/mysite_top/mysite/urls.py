d69c5f6a08aebf4a8f5ba508095fb6de46f48f49
ilyanosovsky <imnosovsky@gmail.com>
2023-06-04 12:28:01 +0300

    working in class with MySite

--- Week5/mysite_top/mysite/urls.py
+++ Week5/mysite_top/mysite/urls.py
@@ -1,28 +1,15 @@
-"""
-URL configuration for mysite project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from polls.views import posts, profile, addresses, add_category_view, add_post_view
+from polls.views import posts, profile, addresses, add_category_view, add_post_view, AddCategoryView, AddPostView, PostList
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("posts/", posts),
-    path("profile/", profile),
-    path("addresses/", addresses),
-    path("add_category/", add_category_view),
-    path("add_post/", add_post_view),
+    # path("posts/", posts, name="posts"),
+    path("posts/", PostList.as_view(), name="posts"),
+    path("profile/", profile, name="profile"),
+    path("addresses/", addresses, name="addresses"),
+    # path("add_category/", add_category_view, name="add_category"),
+    path("add_category/", AddCategoryView.as_view(), name="add_category"),
+    # path("add_post/", add_post_view,  name="add_post"),
+    path("add_post/", AddPostView.as_view(),  name="add_post"),
 ]
--- Week5/mysite_top/polls/views.py
+++ Week5/mysite_top/polls/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Post, Category, Email, Person, Address
 from .forms import CategoryForm, PostForm, SearchForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.db.models import Q
 
 current_user = Person.objects.get(first_name="Ivan")
 
@@ -15,7 +19,27 @@ def add_post_view(request):
     context = {'form': post_form}
     return render(request, "add_post.html", context)
 
+class AddPostView(CreateView):
+    model = Post
+    template_name = "add_post.html"
+    # fields = "__all__"
+    form_class = PostForm
+    success_url = reverse_lazy("add_post")
 
+    def get_initial(self):
+        return {'author': current_user,
+                'title': "Default title"}
+
+    def form_valid(self, form):
+        title = form.cleaned_data.get('title')
+        # print("NAME:", name)
+        if title == "Default title":
+            form.instance.title = "DEFAULT TITLE"
+        return super().form_valid(form)
+
+
+
+# function based view
 def add_category_view(request):
 
     if request.method == "POST":
@@ -26,6 +50,21 @@ def add_category_view(request):
     cotegory_form = CategoryForm()
     context = {'form': cotegory_form}
     return render(request, "add_category.html", context)
+
+# class based view / generic view
+class AddCategoryView(CreateView):
+    model = Category
+    template_name = "add_category.html"
+    # fields = "__all__"
+    form_class = CategoryForm
+    success_url = reverse_lazy("add_category")
+
+    def form_valid(self, form):
+        name = form.cleaned_data.get('name')
+        # print("NAME:", name)
+        if name == "Fantasy":
+            form.instance.name = "FANTASY 1000"
+        return super().form_valid(form)
 
 
 
@@ -40,6 +79,27 @@ def posts(request):
     context = {'post_list': posts_all, 'search': search_form}
 
     return render(request, "posts.html", context)
+
+# class based alternative to posts function
+class PostList(ListView):
+    model = Post
+    template_name = "posts.html"
+    context_object_name = "post_list"
+
+    def get_queryset(self):
+        querry = self.request.GET.get('querry', None)
+        if querry:
+            posts_all = Post.objects.filter(Q(title__icontains=querry) | Q(author__first_name__icontains=querry))
+        else:
+            posts_all = Post.objects.all()
+        return posts_all
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search'] = SearchForm()
+        return context
+
+
 
 def profile(request):
 
