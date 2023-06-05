b15de1fa035cc3f86829168ec551f4f01a990f95
ilyanosovsky <imnosovsky@gmail.com>
2023-06-05 23:22:17 +0300

    fixed XP bug

--- Week6/Day1/filmproject_top/accounts/urls.py
+++ Week6/Day1/filmproject_top/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views
-from .views import RegisterView
+from .views import RegisterView, CustomLoginView, ProfileView, profile
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(template_name='login.html'), name='login'),
+    # path('login/', views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('profile/', profile, name='profile'),
+    path('profile/<int:user_id>/', ProfileView.as_view(), name='profile'),
 ]
