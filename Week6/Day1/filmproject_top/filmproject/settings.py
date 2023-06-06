1c2ed56bb0448029d150cd65f92887e3e5f0f6c9
ilyanosovsky <imnosovsky@gmail.com>
2023-06-06 22:23:23 +0300

    trying to do GOLD

--- Week6/Day1/filmproject_top/filmproject/settings.py
+++ Week6/Day1/filmproject_top/filmproject/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "films",
     "bootstrap5",
     "accounts",
+    "crispy_forms",
+    "crispy_bootstrap5"
 ]
 
 MIDDLEWARE = [
@@ -129,6 +131,10 @@ STATICFILES_DIRS = [Path.joinpath(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 LOGIN_REDIRECT_URL = 'profile'
 LOGOUT_REDIRECT_URL = 'login'
