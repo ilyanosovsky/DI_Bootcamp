a30286607378b54f1fed7ad05e9937fbb027126c
ilyanosovsky <imnosovsky@gmail.com>
2023-06-07 16:34:17 +0300

    starting XP

---
+++ Week6/Day4/image_sharing_project_top/image_share/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile')
+    image_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='media')
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='images')
+
+    def __str__(self):
+        return self.title
+
