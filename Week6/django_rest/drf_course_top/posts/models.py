62bb67bc06bce19c8ed1cdddcbf85eb5ea7407f8
ilyanosovsky <imnosovsky@gmail.com>
2023-06-12 13:01:46 +0300

    posts class work

--- Week6/django_rest/drf_course_top/posts/models.py
+++ Week6/django_rest/drf_course_top/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -19,6 +20,7 @@ class Post(models.Model):
     category = models.CharField(max_length=3, choices=CATEGORY_CHOICES)
     publlish_date = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- Week6/django_rest/drf_course_top/posts/serializers.py
+++ Week6/django_rest/drf_course_top/posts/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.HyperlinkedRelatedField(view_name='user-detail', queryset=User.objects.all())
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = ('title', 'category', 'publlish_date', 'last_update', 'author')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id',)
