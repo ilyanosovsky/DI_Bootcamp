c5a49aa6900f3cce29768475667369c1a0f84029
ilyanosovsky <imnosovsky@gmail.com>
2023-06-12 13:02:26 +0300

    weather class work

--- Week6/django_rest/Exercises/weather_report_top/weatherapp/models.py
+++ Week6/django_rest/Exercises/weather_report_top/weatherapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 TYPE_CHOICES = (
     ("sunny", "Sunny"),
@@ -16,4 +17,11 @@ class Report(models.Model):
     
     def __str__(self):
         return f"{self.location} ({self.temperature}°C)"
+    
+class Forecaster(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.user.username}"
+    
     
