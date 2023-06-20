85507c5de25bfc03ddf344230e350b79fe153077
ilyanosovsky <imnosovsky@gmail.com>
2023-06-20 15:50:16 +0300

    finaly finished Ninja

--- Week7/Day3/Exercises/Ninja/script.js
+++ Week7/Day3/Exercises/Ninja/script.js
@@ -41,3 +41,39 @@ function compare(person1, person2) {
 }
 
 compare(person1, person2);
+
+// Exercise 2 : Grade Average
+// Instructions
+// Hint:
+// - This Exercise is trickier then the others. You have to think about its structure before you start coding.
+// - You must use functions to complete this exercise, to do so take a look at tomorrow’s lesson.
+
+// In this exercise we will be creating a function which takes an array of grades as an argument and will console.log the average.
+
+// Create a function called findAvg(gradesList) that takes an argument called gradesList.
+
+// Your function must calculate and console.log the average.
+
+// If the average is above 65 let the user know they passed
+
+// If the average is below 65 let the user know they failed and must repeat the course.
+// Bonus Try and split parts 1,2 and 3,4 of this exercise to two separate functions.
+// Hint One function must call the other.
+
+let gradesList = [55, 77, 88, 95, 63];
+
+function findAvg(gradesList) {
+    let sum = 0;
+    for (let i of gradesList) {
+        sum += i;
+    }
+    let avg = sum / gradesList.length;
+    console.log(avg);
+    if (avg > 65) {
+        console.log('You passed.');
+    } else {
+        console.log('You failed and must repeat the course.');
+    }
+}
+
+findAvg(gradesList);
