7bfcc2846c0a0ee3175ce7fa3002569cda95ff58
ilyanosovsky <imnosovsky@gmail.com>
2023-06-21 13:43:46 +0300

    finished class work

---
+++ Week7/Day4/class.js
@@ -0,0 +1,52 @@
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// // for (let fruit of fruits) {
+// //     console.log(fruit + 's')
+// // }
+
+// for (let i = 0; i < fruits.length; i++) {
+//     fruits[i] += 's'
+// }
+// console.log(fruits)
+
+// function welcome(username) {
+//     const str = `Welcome ${username}`
+//     console.log(str)
+// }
+
+// welcome('Ivan')
+
+// function checkNumber(arr) {
+//     for (let num of arr) {
+//         if (num % 2 === 0) {
+//             console.log(num + ' is even')
+//         } else {
+//             console.log(num + ' is odd')
+//         }
+//     }
+// }
+
+// checkNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+// const isHappy = true;
+// let action;
+
+// if (isHappy) {
+//     action = 'smile'
+// } else {
+//     action = 'cry'
+// }
+
+// console.log(action)
+
+// const isHappy = true;
+// const action = isHappy ? 'smile' : 'cry'
+
+// function checkNumber(arr) {
+//     for (let num of arr) {
+//         console.log(num % 2 === 0 ? num + ' is even' : num + ' is odd')
+//     }
+// }
+
+// checkNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
--- Week7/Day4/script.js
+++ Week7/Day4/script.js
@@ -1,52 +1,78 @@
-// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const username = "Ivan";
+// const isLoggedIn = true;
 
-// // for (let fruit of fruits) {
-// //     console.log(fruit + 's')
-// // }
-
-// for (let i = 0; i < fruits.length; i++) {
-//     fruits[i] += 's'
-// }
-// console.log(fruits)
-
-// function welcome(username) {
-//     const str = `Welcome ${username}`
-//     console.log(str)
+// if (username === "Ivan" && isLoggedIn) {
+//   document.body.firstElementChild.textContent = "Hello, " + username;
 // }
 
-// welcome('Ivan')
+// const color = prompt("Enter color");
+// const title = document.body.firstElementChild;
+// title.style.backgroundColor = color;
 
-// function checkNumber(arr) {
-//     for (let num of arr) {
-//         if (num % 2 === 0) {
-//             console.log(num + ' is even')
-//         } else {
-//             console.log(num + ' is odd')
-//         }
-//     }
+// const fruits = ["apple", "banana", "orange"];
+
+// const username = "Ivan";
+
+// const container = document.body.firstElementChild;
+
+// const paragraph = document.createElement("p");
+
+// const content = document.createTextNode("Hello, " + username);
+
+// paragraph.appendChild(content);
+
+// container.appendChild(paragraph);
+
+// const fruits = ["apple", "banana", "orange"];
+
+// const container = document.getElementById("all-fruits");
+
+// for (let fruit of fruits) {
+//   const newLi = document.createElement("li");
+//   const text = document.createTextNode(fruit);
+//   newLi.appendChild(text);
+//   container.appendChild(newLi);
 // }
 
-// checkNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+// const fruits = ["melon", "pear", "apple"];
 
-// const isHappy = true;
-// let action;
+// // i want to create 1 li per fruit and add it to the ul
 
-// if (isHappy) {
-//     action = 'smile'
-// } else {
-//     action = 'cry'
+// //1. retrieve the ul
+// const container = document.getElementById("all_fruits");
+
+// for (let fruit of fruits) {
+//     //2. create the li
+//     const newLi = document.createElement("li");
+
+//     newLi.classList.add("ocean");
+//     //3. create the text
+//     const text = document.createTextNode(fruit);
+//     //4. append the text to the li
+//     newLi.appendChild(text);
+//     //5. append the li to the ul
+//     container.appendChild(newLi);
 // }
 
-// console.log(action)
+// For each of the questions, find 2 WAYS of accessing :
 
-// const isHappy = true;
-// const action = isHappy ? 'smile' : 'cry'
+// 1. The div DOM node?
 
-// function checkNumber(arr) {
-//     for (let num of arr) {
-//         console.log(num % 2 === 0 ? num + ' is even' : num + ' is odd')
-//     }
-// }
+// console.log(document.body.firstElementChild);
+// console.log(document.body.children[0]);
 
-// checkNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+// // 2. The ul DOM node?
 
+// console.log(document.body.children[1]);
+// console.log(document.body.lastElementChild.previousElementSibling);
+// // 3. The second li (with Pete)?
+
+// console.log(document.body.children[1].children[1]);
+// console.log(document.body.children[1].lastElementChild);
+
+// const allLi = document.getElementsByTagName("li");
+
+// const myDiv = document.getElementById("container");
+
+
+const allParagraphs = document.querySelectorAll("#container p");
