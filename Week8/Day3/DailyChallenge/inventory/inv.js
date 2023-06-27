3322480a3a91f359d0ccec667a19620ba8640b0c
ilyanosovsky <imnosovsky@gmail.com>
2023-06-27 20:30:02 +0300

    finished daily2

--- Week8/Day3/DailyChallenge/inventory/inv.js
+++ Week8/Day3/DailyChallenge/inventory/inv.js
@@ -24,12 +24,13 @@ console.log(getCarHonda(inventory));
 
 // Part II
 
-// Create a function sortCarInventoryByYear(carInventory) that takes a single parameter. carInventory‘s value is an array which is an inventory of cars (see the array of objects below)
+// Create a function sortCarInventoryByYear(carInventory) that takes a single parameter. carInventory‘s 
+// value is an array which is an inventory of cars (see the array of objects below)
 // the function should return an inventory that is sorted by car_year, ascending.
 // Hint : Check out this tutorial on the sort method
 // Use the cars inventory below:
 
-const inventory = [
+const inventory2 = [
   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
   { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
@@ -37,15 +38,8 @@ const inventory = [
   { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
 ];
 
-
-
-
-// The output should be
-
-// [
-//   { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
-//   { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
-//   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
-//   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
-//   { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
-// ];
+function sortCarInventoryByYear(carInventory) {
+    inventory2.sort((a,b) => a["car_year"] - b["car_year"]);
+    console.log(inventory2);
+}
+sortCarInventoryByYear(inventory2);
