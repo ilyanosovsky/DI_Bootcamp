b9145fb56c4272b2ebc14fd2ccf4f95d09c74d91
ilyanosovsky <imnosovsky@gmail.com>
2023-07-05 23:33:00 +0300

    finished mini project

--- Week9/Day4/Exercises/Mini_Project/script.js
+++ Week9/Day4/Exercises/Mini_Project/script.js
@@ -1,43 +1,55 @@
 const randBtn = document.getElementById('randomBtn');
-randBtn.addEventListener('click', getPerson);
+const divElem = document.getElementById('info');
 
-async function getPerson(num) {
-    console.log("start fetching");
+
+function showLoading() {
+    const div = document.createElement("div");
+    div.className = "fa-3x";
+
+    const i = document.createElement("i");
+    i.className = "fa-solid fa-spinner fa-spin-pulse";
+
+    const p = document.createElement("p");
+    p.textContent = "Loading...";
+
+    div.appendChild(i);
+
+    divElem.appendChild(div);
+    divElem.appendChild(p);
+
+}
+
+
+const getPerson = async () => {
     try {
-        const response = await fetch(`https://www.swapi.tech/api/people/${getNum(num)}`)
-        if (response.ok) {
-            const dataPerson = await response.json();
-            console.log(dataPerson);
-            displayPerson(dataPerson)
+        let endpoint = "https://www.swapi.tech/api/people/" + getNum(); //+ getRandomInt(1, 84)
+        const response = await fetch(endpoint);
+        if (response.ok){
+            data = await response.json();
         } else {
-            throw new Error ("issues with fetch")
+            throw new Error("Oh No! That person isn't available.")
         }
-    } catch (error){
-        console.log("ERROR", error);
+        return data.result;
+    } catch (err) {
+        return err;
     }
 }
 
-const divElem = document.getElementById('info');
-const pName = document.getElementById('namePerson');
-const pHeight = document.getElementById('heightPerson');
-const pGender = document.getElementById('genderPerson');
-const pBirth = document.getElementById('birthPerson');
-const pHome = document.getElementById('homePerson');
 
-function displayPerson(dataPerson) {
-    if (dataPerson['result']['properties']["message"] === "not found"){
-        throw new Error ("Cannot Find a Person");
-    } else {
-        const personName = dataPerson['result']['properties']['name'];
-        const personHeight = dataPerson['result']['properties']["height"];
-        const personGender = dataPerson['result']['properties']["gender"];
-        const personBirth = dataPerson['result']['properties']["birth_year"];
-        // const personHome = dataPerson['result']['properties']["homeworld"];
-        pName.textContent = personName;
-        pHeight.textContent = `Height: ${personHeight}`;
-        pGender.textContent = `Gender: ${personGender}`;
-        pBirth.textContent = `Birth Year: ${personBirth}`;
-        // pHome.textContent = personHome;
+const getPlanet = async (id) => {
+    try {
+        let endpoint = "https://www.swapi.tech/api/planets/" + id;
+        const response = await fetch(endpoint);
+        
+        if (response.ok){
+            data = await response.json();
+        } else {
+            throw new Error("Something went wrong...")
+        }
+        console.log(data.result.properties.name);
+        return data.result.properties.name;
+    } catch (err) {
+        return err;
     }
 }
 
@@ -45,3 +57,50 @@ function getNum () {
     let num = Math.floor(Math.random() * 83) + 1;
     return num
 }
+
+randBtn.addEventListener("click", function (event) {
+    divElem.textContent = "";
+
+    ( async () => {
+        showLoading();
+        // await showLoading();
+        let character = await getPerson();
+
+        if (character instanceof Error) {
+            const error = document.createElement("h1");
+            error.textContent = character.message;
+            divElem.textContent = "";
+            divElem.appendChild(error);
+        } else {
+            character = character.properties;
+
+            const planetId = /[^/]*$/.exec(character.homeworld)[0];
+
+            let planet = await getPlanet(planetId);
+
+            console.log(planet);
+
+            const name = document.createElement("h1");
+            name.textContent = character.name;
+
+            const height = document.createElement("p");
+            height.textContent = `Height: ${character.height}`;
+
+            const gender = document.createElement("p");
+            gender.textContent = `Gender: ${character.gender}`;
+
+            const birthYear = document.createElement("p");
+            birthYear.textContent = `Birth Year: ${character.birth_year}`;
+
+            const homeWorld = document.createElement("p");
+            homeWorld.textContent = `Home World: ${planet}`;
+
+            divElem.textContent = "";
+            divElem.appendChild(name);
+            divElem.appendChild(height);
+            divElem.appendChild(gender);
+            divElem.appendChild(birthYear);
+            divElem.appendChild(homeWorld);
+        }
+    } )();
+ });
