500e846ace7890bc616482d7162885ac05502361
ilyanosovsky <imnosovsky@gmail.com>
2023-07-11 15:17:07 +0300

    finished xp

---
+++ Week10/Day3/XP/fs.js
@@ -0,0 +1,23 @@
+// Exercise 1 : Reading From A File In Node.JS
+// Instructions
+// Create a text file and write something inside (example: ‘Some Text To See’)
+// Create an fs.js file, and inside use the Node JS File System to read the text file and console.log the output in the terminal
+
+const fs = require('fs');
+
+fs.readFile('text.txt', 'utf-8', (err,data) => {
+    if (err) return console.log(err);
+    console.log(data);
+});
+
+fs.writeFile('newtext.txt', 'Ivan develops everything everywhere all day long', 'utf-8', (err) => {
+    if (err) return console.log(err);
+})
+
+fs.appendFile('newtext.txt', "and buy falafeles", 'utf-8', (err) => {
+    if (err) return console.log(err);
+})
+
+fs.unlink('text.txt', (err) => {
+    if (err) return console.log(err);
+});
