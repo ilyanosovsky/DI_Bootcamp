e5c1743baac40f9ad57625986131e6a73db19ea1
ilyanosovsky <imnosovsky@gmail.com>
2023-07-21 17:20:43 +0300

    finished robofriends project

--- Week11/Day1/landingpage/landing/src/index.js
+++ Week11/Day1/landingpage/landing/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './containers/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-          <App />
+  <div>
+    <App />
+  </div>
 );
 
 reportWebVitals();
