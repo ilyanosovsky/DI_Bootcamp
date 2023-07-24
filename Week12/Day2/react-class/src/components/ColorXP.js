98ad227c883e70a69a0a036fa8252f8a42f06d6a
ilyanosovsky <imnosovsky@gmail.com>
2023-07-24 17:18:47 +0300

    finished XP lifecircle

--- Week12/Day2/react-class/src/components/ColorXP.js
+++ Week12/Day2/react-class/src/components/ColorXP.js
@@ -1,30 +1,80 @@
-import {useState, useEffect} from 'react';
+import { Component } from 'react';
 
-const ColorXP = () => {
-    const [favoriteColor, setFavColor] = useState('red')
-    const [color, setColor] = useState('');
-
-    useEffect(() => {
-        alert('useEffect reached');
-        setFavColor('yellow')
-    }, []);
-
-    useEffect(() => {
-        if (color !== '')
-        setFavColor(color)
-    }, [color])
-
-    const changeColor = () => {
-        setFavColor(favoriteColor==='red'? 'blue' : 'red')
+class Child extends Component {
+    componentWillUnmount() {
+        alert("The component named Header is about to be unmounted.");
     }
 
-    return (
-        <>
-            <h1>My Favorite Color is {favoriteColor}</h1>
-            <input onChange={(e) => setColor(e.target.value)}/>
-            <button onClick={changeColor}>Change Color</button>
-        </>
-    )
+    render() {
+        return (
+            <h1>Hello World!</h1>
+        )
+    }
 }
 
+class ColorXP extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            favoriteColor: "red",
+            show: true
+        };
+      }
+
+    changeColor = () => {
+        this.setState(
+            {
+              favoriteColor: "blue"
+            }
+        )
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState(
+                {
+                  favoriteColor: "yellow"
+                }
+            );
+          }, 5000);
+    }
+
+    // shouldComponentUpdate() {
+    //     return false;
+    // }
+
+    getSnapshotBeforeUpdate() {
+        console.log("in getSnapshotBeforeUpdate");
+    }
+
+    componentDidUpdate() {
+        console.log("after update");
+    }
+
+    deleteHeader = () => {
+        this.setState(
+            {
+                show: false
+            }
+        )
+    }
+
+    render() {
+        let comp;
+        if (this.state.show) {
+            comp = <Child />;
+          }
+
+      return (
+        <div>
+            {comp ? comp : <h1>My favorite color is <i>{ this.state.favoriteColor }</i></h1>}
+
+            <button onClick={ this.deleteHeader }>Delete Header</button>
+            
+            <button onClick={ this.changeColor }>Change color</button>
+        </div>
+      )
+    }
+}
+  
 export default ColorXP;
